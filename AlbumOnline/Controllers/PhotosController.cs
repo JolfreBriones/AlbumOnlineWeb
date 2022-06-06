09c859413a601062d0880d4bc735c2fbd03b236f
JolfreBriones <jolfrebriones@gmail.com>
2022-06-06 00:14:48 -0600

    Agregar archivos de proyecto.

---
+++ AlbumOnline/Controllers/PhotosController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using AlbumOnline.Models;
+
+namespace AlbumOnline.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private AlbumOnlineModelContainer db = new AlbumOnlineModelContainer();
+
+        // GET: Photos
+        public ActionResult Index()
+        {
+            var photos = db.Photos.Include(p => p.Tipo);
+
+            return View(photos.ToList());
+        }
+
+        // GET: Photos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public ActionResult Create()
+        {
+            ViewBag.TipoId = new SelectList(db.Tipos, "Id", "Descrip");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Img,User,Fecha,Status,TipoId")] Photo photo)
+        {
+            HttpPostedFileBase fileBase = Request.Files[0];
+            WebImage webImage = new WebImage(fileBase.InputStream);
+            photo.Img = webImage.GetBytes();
+            //byte[] imagenBinaria = webImage.GetBytes();
+            //photo.Img = Convert.ToBase64String(imagenBinaria);
+            
+
+            if (ModelState.IsValid)
+            {
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TipoId = new SelectList(db.Tipos, "Id", "Descrip", photo.TipoId);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TipoId = new SelectList(db.Tipos, "Id", "Descrip", photo.TipoId);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Img,User,Fecha,Status,TipoId")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(photo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TipoId = new SelectList(db.Tipos, "Id", "Descrip", photo.TipoId);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
